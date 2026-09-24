360f3208bb3691fc72af334e9b6bb4e4a0344d9c
RazorVA69 <Razorva327@gmail.com>
2026-09-24 09:12:33 +0600

    Add files via upload

--- extensions/mxplayerad/src/main/java/app/ftl/extension/mxplayerad/ModSettingsRow.java
+++ extensions/mxplayerad/src/main/java/app/ftl/extension/mxplayerad/ModSettingsRow.java
@@ -2,30 +2,12 @@ package app.ftl.extension.mxplayerad;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.util.TypedValue;
-import android.view.Gravity;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.LinearLayout;
 
-public class ModSettingsRow extends TextView {
+public class ModSettingsRow extends LinearLayout {
     public ModSettingsRow(Context context, AttributeSet attrs) {
         super(context, attrs);
-
-        setText("Mod Settings");
-        setTextSize(16f);
-        setGravity(Gravity.CENTER_VERTICAL);
-
-        float density = context.getResources().getDisplayMetrics().density;
-        int horizontal = (int) (20 * density + 0.5f);
-        int vertical = (int) (16 * density + 0.5f);
-        setPadding(horizontal, vertical, horizontal, vertical);
-
-        TypedValue background = new TypedValue();
-        if (context.getTheme().resolveAttribute(android.R.attr.selectableItemBackground, background, true)
-            && background.resourceId != 0) {
-            setBackgroundResource(background.resourceId);
-        }
-
         setClickable(true);
         setOnClickListener(new View.OnClickListener() {
             @Override
